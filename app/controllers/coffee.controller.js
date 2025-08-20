d6def83b3e933d032d8b2e1192ca726058dd9de9
Tony Saes <saestony.dev@gmail.com>
2025-08-20 15:28:20 +0200

    create view for each coffee with MVC

--- app/controllers/coffee.controller.js
+++ app/controllers/coffee.controller.js
@@ -7,5 +7,13 @@ export default {
             return next();
         }
         res.render("catalog", { coffees, title: "Liste des cafés", cssFile: "catalog.css" });
+    },
+
+    detail: async (req, res, next) => {
+        const coffee = await coffeeModels.getCoffeeByReference(req.params.reference);
+        if (!coffee) {
+            return next();
+        }
+        res.render("coffee", { coffee, title: coffee.name, cssFile: "coffee.css" });
     }
 }
--- app/models/coffee.models.js
+++ app/models/coffee.models.js
@@ -10,6 +10,17 @@ export default {
             throw error;
         }
     },
+
+    async getCoffeeByReference(reference) {
+        try {
+            const res = await client.query("SELECT coffee.reference, coffee.name, coffee.price, country.name AS country_name, coffee.description, STRING_AGG(taste.type, ', ' ORDER BY taste.type) AS coffee_type FROM coffee JOIN country ON country.country_id = coffee.country_id join belong ON belong.reference = coffee.reference join taste ON taste.taste_id = belong.taste_id WHERE coffee.reference = $1 group by coffee.name, coffee.reference, country.name, coffee.description, coffee.price", [reference]);
+            return res.rows[0];
+        } catch (error) {
+            console.error('Erreur lors de la récupération du café :', error.message);
+            throw error;
+        }
+    },
+
     async getCoffeesNameForHome() {
         try {
             const res = await client.query('SELECT name, reference FROM coffee LIMIT 3');
